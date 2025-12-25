9cc51c3f190882e7098fe24603a992388f82e42e
MD-Aftab-Hossain <md.aftab.hn@gmail.com>
2025-12-26 00:57:00 +0600

    trial

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1 className="bg-accent">Outlet Successfully Worked</h1>
+      <h1 className="bg-accent">Outlet Successfully Worked. tasty food is not always harmful. don't mind leader. it is a trial.</h1>
     </div>
   );
 };
