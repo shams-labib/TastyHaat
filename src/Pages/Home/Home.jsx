25311eeabdf9f48eaac9d37eee572f3c13785bd3
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-12-26 01:41:43 +0600

    Designed Menu Cards

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import TopSlider from "../Hero/Slider/TopSlider";
 import Hero from "../Hero/Hero";
 import WhyChooseUs from "../Hero/WhyChooseUs/WhyChooseUs";
+import MenuCards from "./MenuCards";
 
 const Home = () => {
   return (
     <div>
       <TopSlider></TopSlider>
       <WhyChooseUs></WhyChooseUs>
+      <MenuCards></MenuCards>
     </div>
   );
 };
