d4e6dad3d93ce502d00c1193e31ba7a32a2ecb89
shams al labib <samslabib19@gmail.com>
2025-12-25 15:08:59 +0600

    herocard designed success

---
+++ src/Pages/HeroCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroCard = () => {
+  return (
+    <div>
+      <h1>Hero card</h1>
+    </div>
+  );
+};
+
+export default HeroCard;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Hero from "../Hero";
+import HeroCard from "../HeroCard";
 
 const Home = () => {
   return (
     <div>
       <h1 className="bg-accent">Outlet Successfully Worked</h1>
+      <Hero></Hero>
+      <HeroCard></HeroCard>
     </div>
   );
 };
